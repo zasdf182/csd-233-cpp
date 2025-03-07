9e522e7a14956d28d08d4d9b233ec9e3f4114c1f
zasdf182 <35350192+zasdf182@users.noreply.github.com>
2025-03-07 04:36:54 -0800

    docs: add missing @brief to comments in str.cpp and str.hpp

--- src/5c_projSearchTree/str.hpp
+++ src/5c_projSearchTree/str.hpp
@@ -5,14 +5,14 @@ using namespace std;
 namespace Str {
 
 ////////////////////////////////////////////////////////////////////////////////
-/// Reads an entire file into a string.
+/// @brief Reads an entire file into a string.
 /// Returns a string list of the file contents split at whitespaces.
 /// Returns an empty list if file is empty or broken.
 ////////////////////////////////////////////////////////////////////////////////
 static list<string> GetWordsFromFile(string filePath);
 
 ////////////////////////////////////////////////////////////////////////////////
-/// Trims leading and trailing whitespace from a string.
+/// @brief Trims leading and trailing whitespace from a string.
 /// Mutates and returns the original input string.
 ////////////////////////////////////////////////////////////////////////////////
 static string Trim(string input);
