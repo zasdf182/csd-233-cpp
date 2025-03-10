d2b7eb51ed6f52edb15f5bf9e4e0bf1ad83a19a6
zasdf182 <35350192+zasdf182@users.noreply.github.com>
2025-03-10 04:27:37 -0700

    build: setup project dir structure and add modules from previous projects

---
+++ src/6_projMazePart1/_main.hpp
@@ -0,0 +1,5 @@
+#include <iostream>
+#include <stdio.h>
+#include <math.h>
+#include "lib/include/opencv.hpp"
+#include "types.hpp"
---
+++ src/6_projMazePart1/lib/include/opencv.hpp
@@ -0,0 +1,6 @@
+#ifndef OPENCV_HPP
+#define OPENCV_HPP
+#include "opencv2/core.hpp"
+#include "opencv2/imgproc.hpp"
+#include "opencv2/highgui.hpp"
+#endif
