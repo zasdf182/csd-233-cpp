33d53d5f14208e73f2a233c46021a4dec29cdc49
zasdf182 <35350192+zasdf182@users.noreply.github.com>
2025-03-10 18:48:36 -0700

    feat: add square class

---
+++ src/6_projMazePart1/modules/maze/square.hpp
@@ -0,0 +1,35 @@
+#ifndef SQUARE_HPP
+#define SQUARE_HPP
+#include "../../include/opencv.hpp"
+namespace Draw {
+
+
+
+////////////////////////////////////////////////////////////////////////////////
+/// @brief A 2D shape to be written to an OpenCV raster matrix.
+///        Set properties then call Draw() to render to window.
+////////////////////////////////////////////////////////////////////////////////
+class Square {
+    public: //Static properties.
+        static const int lineType = cv::LINE_4;
+
+    public: //Mutable properties.
+        int x;
+        int y;
+        int width;
+        int height;
+        int blue;
+        int green;
+        int red;
+        int thickness;
+
+    public: //Constructors and destructors.
+        Square(int x, int y, int width, int height):
+            x(x), y(y), width(width), height(height),
+            blue(255), green(255), red(255), thickness(2) {};
+
+    public: //OpenCV drawing methods.
+        virtual void Draw(std::string wndname, cv::Mat raster);
+};
+}
+#endif
